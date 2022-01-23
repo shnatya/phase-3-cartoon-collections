6c8d85332ae3dc3b9ce180f37642c4104ad76438
Anastasia Shatokhina <shnatya@gmail.com>
2022-01-22 21:10:22 -0800

    commit tree e6e471d3c78ddb24fae6ce2e498d050824779137

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,43 @@
-def roll_call_dwarves# code an argument here
+require "pry"
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.map.with_index {|name, index| puts "#{index+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  new_arr = calls.map do |call|
+    if call.length > 4
+      true
+    else 
+      false
+    end
+  end
+  new_arr.include?(true)
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  i=0
+  found = nil
+  while i < cheese_types.length
+    #binding.pry
+    if snacks.include?(cheese_types[i])
+      #binding.pry
+      found = cheese_types[i]
+      #binding.pry
+      break
+    else
+      #binding.pry
+      i += 1
+    end
+  end
+  found
 end
